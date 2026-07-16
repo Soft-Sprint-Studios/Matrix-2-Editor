e3e4b60ce6bdeafb762514619de4fcc0d7dfc4ae
Jdjd Gaming <80889633+valina354@users.noreply.github.com>
2026-07-16 11:14:26 +0300

    change

--- Sledge.BspEditor.Editing/Components/Compile/BuildParametersPanel.cs
+++ Sledge.BspEditor.Editing/Components/Compile/BuildParametersPanel.cs
@@ -11,7 +11,6 @@ namespace Sledge.BspEditor.Editing.Components.Compile
 	public partial class BuildParametersPanel : UserControl
 	{
 		private CompileTool _tool;
-		private DataTable _data;
 
 		public CompileTool Tool
 		{
@@ -27,165 +26,15 @@ namespace Sledge.BspEditor.Editing.Components.Compile
 		public BuildParametersPanel(bool editable = false)
 		{
 			InitializeComponent();
-
-			_data = new DataTable();
-			_data.Columns.Add("Flag", typeof(string));
-			if (!editable)
-			{
-
-				_data.Columns.Add("Selected", typeof(bool));
-				_data.Columns.Add("Name", typeof(string));
-				_data.Columns.Add("Value", typeof(object));
-
-				_data.RowChanged += (s, e) => UpdatePreview();
-
-				var flagColumn = new DataGridViewTextBoxColumn
-				{
-					AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells,
-					DataPropertyName = "Flag",
-					ReadOnly = true
-				};
-				dataTable.Columns.Add(flagColumn);
-
-				var checkboxColumn = new DataGridViewCheckBoxColumn
-				{
-					AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells,
-					Width = 20,
-					DataPropertyName = "Selected"
-				};
-				dataTable.Columns.Add(checkboxColumn);
-
-				var nameColumn = new DataGridViewTextBoxColumn
-				{
-					AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells,
-					Width = 5,
-					DataPropertyName = "Name",
-					ReadOnly = true
-				};
-				dataTable.Columns.Add(nameColumn);
-
-				var valueColumn = new DataGridViewTextBoxColumn
-				{
-					AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill,
-					DataPropertyName = "Value"
-				};
-				dataTable.Columns.Add(valueColumn);
-			}
-			else
-			{
-				var flagColumn = new DataGridViewTextBoxColumn
-				{
-					AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill,
-					DataPropertyName = "Flag",
-				};
-				dataTable.Columns.Add(flagColumn);
-				this.dataTable.AllowUserToAddRows = true;
-				this.dataTable.AllowUserToDeleteRows = true;
-			}
-
-			dataTable.DataSource = _data;
 		}
 
 		private void SetTool(CompileTool tool)
 		{
 			_tool = tool;
-
-			_data.Rows.Clear();
-			foreach (var parameter in tool.Parameters)
-			{
-				if (!tool.Custom)
-				{
-					_data.Rows.Add(parameter.Flag, false, parameter.Name, parameter.Value);
-				}
-			}
-
-			dataTable.Update();
-			UpdatePreview();
 		}
 
-		private string GetArguments()
-		{
-			var list = new List<string>();
+        private string GetArguments() => txtPreviewText.Text;
 
-			if (((DataTable)dataTable.DataSource).Columns.Count > 1)
-			{
-				foreach (DataRow row in ((DataTable)dataTable.DataSource).Rows)
-				{
-					if (Convert.ToBoolean(row[1]))
-					{
-						var key = Convert.ToString(row[0]);
-						var val = Convert.ToString(row[3]).Trim();
-						list.Add(key);
-						if (val.Length > 0) list.Add(val);
-					}
-				}
-				return String.Join(" ", list);
-			}
-			else
-			{
-				foreach (DataRow row in ((DataTable)dataTable.DataSource).Rows)
-				{
-					list.Add(Convert.ToString(row[0]));
-				}
-				return String.Join("\n", list);
-			}
-		}
-
-		private void SetArguments(string arguments)
-		{
-			var kvs = arguments.SplitWithQuotes().ToList();
-			if (((DataTable)dataTable.DataSource).Columns.Count > 1)
-			{
-				foreach (DataRow row in ((DataTable)dataTable.DataSource).Rows)
-				{
-					var key = Convert.ToString(row[0]);
-					var idx = kvs.IndexOf(key);
-
-					if (idx >= 0)
-					{
-						var val = "";
-						if (idx < kvs.Count - 1)
-						{
-							var next = kvs[idx + 1];
-							if (next[0] != '-') val = next;
-						}
-
-						row[1] = true;
-						row[3] = val;
-					}
-					else
-					{
-						row[1] = false;
-					}
-				}
-			}
-			else
-			{
-				if (!String.IsNullOrEmpty(arguments) && !String.IsNullOrWhiteSpace(arguments))
-				{
-					var commands = arguments.Split('\n');
-					foreach (var command in commands)
-					{
-						if (!String.IsNullOrEmpty(command) && !String.IsNullOrWhiteSpace(command))
-							_data.Rows.Add(command);
-					}
-				}
-			}
-			dataTable.Update();
-			UpdatePreview();
-		}
-
-		private void UpdatePreview()
-		{
-			txtPreviewText.Text = GetArguments();
-		}
-
-		private void StateChanged(object sender, EventArgs e)
-		{
-			if (!(dataTable.CurrentCell.Value is bool)) return;
-
-			dataTable.CommitEdit(DataGridViewDataErrorContexts.Commit);
-			_data.AcceptChanges();
-		}
+        private void SetArguments(string arguments) => txtPreviewText.Text = arguments;
 	}
 }
