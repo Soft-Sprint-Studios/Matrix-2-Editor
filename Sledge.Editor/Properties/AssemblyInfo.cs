7357710b2b6acf6424ccac61436ad40f6b0ce22f
Jdjd Gaming <80889633+valina354@users.noreply.github.com>
2026-07-30 20:11:13 +0300

    rename stuff

--- Sledge.Editor/Properties/AssemblyInfo.cs
+++ Sledge.Editor/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MatrixED.Editor")]
+[assembly: AssemblyTitle("MatrixEditor")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MatrixED.Editor")]
+[assembly: AssemblyProduct("MatrixEditor")]
 [assembly: AssemblyCopyright("Copyright ©  2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
